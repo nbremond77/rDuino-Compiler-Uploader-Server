cc814b48f3dab181b12613d5c7ffeef143618384
Bernard REMOND <nbremond@laposte.net>
2016-05-10 16:13:21 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -53,11 +53,11 @@ def install_library():
     theResult = ""
      # arduino --install-library "Bridge:1.0.0"
     if request.method == 'POST':
-      theLibrary = request.form['library']
-      myCmd = myArduinoExe+" "+myInstallLibrary+" "+theLibrary
-      print("%s ..." % myCmd)
-      #theResult = exec(myCmd)
-      print(" Done. Result:%s\n" % theResult)
+        theLibrary = request.form['library']
+        myCmd = myArduinoExe+" "+myInstallLibrary+" "+theLibrary
+        print("%s ..." % myCmd)
+        #theResult = exec(myCmd)
+        print(" Done. Result:%s\n" % theResult)
     return render_template('install_library.html', cmd=myCmd, result=theResult)
 
 
@@ -67,11 +67,11 @@ def install_boards():
     theResult = ""
      # arduino --install-boards "arduino:sam"
     if request.method == 'POST':
-      theBoard = request.form['board']
-      myCmd = myArduinoExe+" "+myInstallBoard+" "+theBoard
-      print("%s ..." % myCmd)
-      #theResult = exec(myCmd)
-      print(" Done. Result:%s\n" % theResult)
+        theBoard = request.form['board']
+        myCmd = myArduinoExe+" "+myInstallBoard+" "+theBoard
+        print("%s ..." % myCmd)
+        #theResult = exec(myCmd)
+        print(" Done. Result:%s\n" % theResult)
     return render_template('install_boards.html', cmd=myCmd, result=theResult)
 
 
@@ -80,15 +80,15 @@ def upload_file():
     theResult = ""
     theCode = "??"
     if request.method == 'POST':
-      myFileName = 'uploaded_file.txt'
-      theCode = "??"
-      f = request.files['the_file']
-      f.save(myTempDirectory + myFileName)
+        myFileName = 'uploaded_file.txt'
+        theCode = "??"
+        f = request.files['the_file']
+        f.save(myTempDirectory + myFileName)
       
-      myCmd = myArduinoExe+" "+myBoardOptions+" "+myBoard+" "+myTargetOption+" "+myTarget+" "+myOtherOptions+" "+myCompileAndUploadOption+" "+myTempDirectory+myFileName
-      print("%s ..." % myCmd)
-      #theResult = exec(myCmd)
-      print(" Done.\n")
+        myCmd = myArduinoExe+" "+myBoardOptions+" "+myBoard+" "+myTargetOption+" "+myTarget+" "+myOtherOptions+" "+myCompileAndUploadOption+" "+myTempDirectory+myFileName
+        print("%s ..." % myCmd)
+        #theResult = exec(myCmd)
+        print(" Done.\n")
     
     return render_template('main.html', code=theCode, result=theResult, cmd=myCmd)
 
