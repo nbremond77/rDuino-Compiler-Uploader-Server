ee5a6320a686483c5d03a75a02cc3f7ce088c889
Bernard REMOND <nbremond@laposte.net>
2016-05-10 15:21:10 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -11,12 +11,28 @@
 #
 # --------------------------------------------------------------------
 
+ # from http://flask.pocoo.org/ 
 from flask import Flask
+
+# Configuration data
+myPort = 888
+myArduinoExe = "arduino"
+myCompileAndUploadOption= "-upload"
+myTempDirectory = "/tmp"
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#@app.route('/')
+#def hello_world():
+#    return 'Hello World!'
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/tmp/uploaded_file.txt')
+        
+    return 'Hello World!'
+        
 if __name__ == '__main__':
-    app.run()
+    app.run(port=myPort)
