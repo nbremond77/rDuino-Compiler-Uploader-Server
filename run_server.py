7827f7a6d9bb7454bc8cf1b9eb3e774e0e882835
nbremond77 <nbremond@laposte.net>
2016-05-30 23:39:50 +0200

    Background color depand on success

--- run_server.py
+++ run_server.py
@@ -61,8 +61,11 @@ myTempDirectory = "/tmp/uploaded_file"
 myCmd = ""
 theResult = ""
 theError = ""
+theReturnCode = 0
 compileTime = datetime.now()
 myFileName = "uploaded_file.ino"
+
+myProc = None
 
 
 # Define the main application
@@ -75,12 +78,14 @@ app = Flask(__name__)
 def install_library():
     global theResult
     global theError
+    global theReturnCode
     global myCmd
 
     if request.method == 'POST':
         myCmd = ""
         theResult = ""
         theError  =""
+        theReturnCode = 0        
         theLibrary = request.form['library']
         # arduino --install-library "Bridge:1.0.0"
         myCmd = myArduinoExe+" "+myInstallLibrary+" "+theLibrary
@@ -102,12 +107,14 @@ def install_library():
 def install_board():
     global theResult
     global theError
+    global theReturnCode
     global myCmd
 
     if request.method == 'POST':
         myCmd = ""
         theResult = ""
         theError = ""
+        theReturnCode = 0        
         theBoard = request.form['board']
         # arduino --install-boards "arduino:sam"
         myCmd = myArduinoExe+" "+myInstallBoard+" "+theBoard
@@ -127,43 +134,95 @@ def install_board():
 @app.route('/set_target', methods=['GET', 'POST'])
 def set_target():
     global myTarget
+    global theResult
+    global theError
+    global theReturnCode
+
     if request.method == 'POST':
         myTarget = request.form['target']
         print("Taget set to:[%s]\n" % (myTargetRoot + myTarget))
+        theResult = ""
+        theError = ""
+        theReturnCode = 0        
     return redirect('/')
     
 # Define the board
 @app.route('/set_board', methods=['GET', 'POST'])
 def set_board():
     global myBoard
+    global theResult
+    global theError
+    global theReturnCode
+
     if request.method == 'POST':
         myBoard = request.form['board']
         print("Board set to:[%s]\n" % myBoard)
+        theResult = ""
+        theError = ""
+        theReturnCode = 0        
     return redirect('/')
     
 # Define the board
 @app.route('/set_option', methods=['GET', 'POST'])
 def set_option():
     global myOption
+    global theResult
+    global theError
+    global theReturnCode
+
     if request.method == 'POST':
         myOption = request.form['option']
         print("Option set to:[%s]\n" % myOption)
+        theResult = ""
+        theError = ""
+        theReturnCode = 0        
     return redirect('/')
 
 @app.route('/get_result')
 def get_result():
     global theResult
     global compileTime
+    global theReturnCode
+    global myProc
+    
+    if myProc:
+        for line in myProc.stdout:
+            #out = str(line.rstrip())
+            result = line.decode('utf-8')
+            theResult = theResult + result.replace("\n","<br/>")
+            print("   ++ Result:[%s]\n" % result)
+        #myProc.stdout.flush()
+       
     theTime = datetime.now()
 #    print("result: %s\n" % theTime.strftime("%A %d %B %Y %H:%M:%S") + theResult)
 #    print("compile time : %s\n" % compileTime.strftime("%A %d %B %Y %H:%M:%S"))
-    return jsonify(date=theTime.strftime("%A %d %B %Y %H:%M:%S"),  compileTime =  compileTime.strftime("%A %d %B %Y %H:%M:%S"), result=theResult)
+    return jsonify(date=theTime.strftime("%A %d %B %Y %H:%M:%S"),  compileTime =  compileTime.strftime("%A %d %B %Y %H:%M:%S"), result=theResult,   returnCode=theReturnCode)
 #    return time.strftime("%A %d %B %Y %H:%M:%S") + theResult
 
 @app.route('/get_error')
 def get_error():
     global theError
-    return jsonify(error = theError)
+    global myProc
+    global theReturnCode
+
+    if myProc:
+        for line in myProc.stderr:
+            #err = str(line.rstrip())
+            error = line.decode('utf-8')
+            theError = theError + error.replace("\n","<br/>")
+            print("   ++ Error:[%s]\n" % error)
+        #myProc.stderr.flush()    
+
+        status = myProc.poll()
+        if status is not None: # End of subprocess
+            if (theError.find("can't open") >= 0):
+                theReturnCode = -2 # Error
+            elif (theError.find("stk500_getsync") >= 0):
+                theReturnCode = -2 # Error
+            else:
+                theReturnCode = 2 # Good
+            
+    return jsonify(error = theError,  returnCode=theReturnCode)
 #    return theError
     
 # Main page, and process code compile and upload requests
@@ -171,14 +230,17 @@ def get_error():
 def main_page():
     global theResult
     global theError
+    global theReturnCode
     global myCmd
     global myFileName
     global compileTime
+    global myProc
     
     if request.method == 'POST':
 
         theResult = ""
         theError = ""
+        theReturnCode = 0
         theCode = ""
         myCmd = ""
         myFileName = 'uploaded_file.ino'
@@ -197,21 +259,37 @@ def main_page():
         print("\nThe shell command:\n%s\n" % myCmd)
 
 #        theResult = os.system(myCmd)
-        proc = subprocess.Popen(myCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        result = out.decode(encoding='UTF-8')
-#        theResult = result
-        theResult = result.replace("\n","<br/>")
-        error = err.decode(encoding='UTF-8')
-        theError = error.replace("\n","<br/>")
-#        theError = error
+        myProc = subprocess.Popen(myCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+#        myProc = subprocess.Popen(myCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
-        print("\nThe output of the compiler-linker-uploader:\n%s\n" % result)
-        print("\nThe errors :\n%s\n" % error)
+        # Subprocess blocking
+        #(out, err) = myProc.communicate()
+        
+        # Non blocking
+            
+        if myProc:
+            for line in myProc.stdout:
+                #out = str(line.rstrip())
+                result = line.decode('utf-8')
+                theResult = theResult + result.replace("\n","<br/>")
+            print("-- Result:[%s]\n" % theResult)
+            #myProc.stdout.flush()
+            for line in myProc.stderr:
+                #err = str(line.rstrip())
+                error = line.decode('utf-8')
+                theError = theError + error.replace("\n","<br/>")
+            print("-- Error:[%s]\n" % theError)
+            #myProc.stderr.flush()  
+            
+#        result = out.decode(encoding='UTF-8')
+#        theResult = result.replace("\n","<br/>")
+#        error = err.decode(encoding='UTF-8')
+#        theError = error.replace("\n","<br/>")
+
+        print("\nThe output of the compiler-linker-uploader:\n%s\n" % theError)
+        print("\nThe errors :\n%s\n" % theError)
+        print("\nThe Return Code :\n%s\n" % theReturnCode)
         print(" Done.\n")
-        
-#        return result
-#        return redirect('/')        
     
     return render_template('main.html', thePort=myPort, theBoardList=boardList, theBoard=myBoard, theTargetList=targetList, theTarget=myTarget, theOptionList=myOptionList, theOption=myOption, theTempFile=myTempDirectory+separator+myFileName, result=theResult, error=theError)
 
