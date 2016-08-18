accf9f6e8ffc84643477c82943eed472e7d419b1
Bernard REMOND <nbremond@laposte.net>
2016-08-18 17:56:04 +0200

    Add files via upload

--- run_server.old.py
+++ run_server.old.py
@@ -3,7 +3,7 @@
 
 
 # --------------------------------------------------------------------
-# rDuino_Uploader_Server : Mini server to compile and upload
+# rDuino-Compiler-Uploader-Server : Mini server to compile and upload
 # a code to the target board.
 #
 # This program serves HTTP requests on port 888
@@ -15,18 +15,17 @@
 # --------------------------------------------------------------------
 
 import os
-import sys
 import subprocess
 from datetime import datetime, date, time
-import optparse
 
 # from http://flask.pocoo.org/ 
 from flask import Flask, abort, redirect, url_for, request, render_template,  Markup,  make_response,  session,  escape,  jsonify
 
 # Configuration data
-#debugMode = 0
-
+#myPort = 888
+#myPort = 5000
 myPort = 5005
+#myPort = 1020
 
 #myArduinoExe = "arduino_debug.exe" # Windows
 #myArduinoExe = "Arduino.app/Contents/MacOS/Arduino" # MAC
@@ -45,10 +44,11 @@ myBoard = "arduino:avr:uno"
 
 myTargetOption = "--port"
 #targetList = ["COM1","COM2","COM3","COM4","COM5","COM6"]
-targetList = ["/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyUSB2", "/dev/ttyUSB3", "/dev/ttyACM0", "/dev/ttyACM1", "/dev/ttyACM2", "/dev/ttyACM3"]
+targetList = ["ttyUSB0", "ttyUSB1", "ttyUSB2", "ttyUSB3", "ttyACM0", "ttyACM1", "ttyACM2", "ttyACM3"]
 #myTarget = "COM3"
-myTarget = "/dev/ttyUSB0"
-#myTarget = "/dev/ttyACM0"
+myTargetRoot="/dev/"
+myTarget = "ttyUSB0"
+#myTarget = "ttyACM0"
 
 #myOtherOptions = "--verbose-upload"
 myOptionList = ["", "--verbose-upload",  "--verbose-build",  "--verbose",  "--preserve-temp-files"]
@@ -70,11 +70,9 @@ myFileName = "uploaded_file.ino"
 
 myProc = None
 
-    
+
 # Define the main application
 app = Flask(__name__)
-
-#global app
 
 # Define the routes
 
@@ -145,7 +143,7 @@ def set_target():
 
     if request.method == 'POST':
         myTarget = request.form['target']
-        print("Taget set to:[%s]\n" % (myTarget))
+        print("Taget set to:[%s]\n" % (myTargetRoot + myTarget))
         theResult = ""
         theError = ""
         theReturnCode = 0        
@@ -229,101 +227,10 @@ def get_error():
             
     return jsonify(error = theError,  returnCode=theReturnCode)
 #    return theError
-
-
-# Open the code in the Arduino IDE
-@app.route('/openIDE', methods=['GET', 'POST'])
-def openIDE():
-    global theResult
-    global theError
-    global theReturnCode
-    global myCmd
-    global myFileName
-    global compileTime
-    global myProc
-    
-    if request.method == 'POST':
-
-        theResult = ""
-        theError = ""
-        theReturnCode = 0
-        theCode = ""
-        myCmd = ""
-        myFileName = "uploaded_file.ino"
-        
-#        tmp = request.data
-        theCode = tmp.decode(encoding='UTF-8')
-        theCode = tmp
-        theFileName = myTempDirectory + separator + myFileName
-        print("The code:\n%s\n" % theCode)
-        print("Try to save the code to a local file %s\n" % theFileName)
-
-        # Write the code to a temp file
-        try:
-            f = open(theFileName, "w")
-            print("Trying...\n")
-#        except (OSError, IOError) as err:
-        except:
-            print("Unable to open file for writing: IOError: %s\n" % sys.exc_info()[0])
-        else:
-            try:
-                print("Writing...\n")
-                f.write(theCode)
-                print("Code written to %s\n" % theFileName)
-#            except  (OSError, IOError) as err:
-            except:
-                print("Unable to write in the file : %s\n" % sys.exc_info()[0])
-            finally:
-                f.close()
-                print("File closed.\n")
-
-
-        # arduino --board arduino:avr:nano:cpu=atmega168 --port /dev/ttyACM0 --upload /path/to/sketch/sketch.ino
-        compileTime = datetime.now()
-        myCmd = myArduinoExe+" "+myBoardOptions+" "+myBoard+" "+myTargetOption+" "+myTarget+" "+myOption+" "+myTempDirectory+separator+myFileName
-        print("\nThe shell command:\n%s\n" % myCmd)
-
-#        theResult = os.system(myCmd)
-        myProc = subprocess.Popen(myCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-#        myProc = subprocess.Popen(myCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        # Subprocess blocking
-        #(out, err) = myProc.communicate()
-        
-        # Non blocking
-        theResult = myCmd + "<br/><br/>"
-            
-        if myProc:
-            for line in myProc.stdout:
-                #out = str(line.rstrip())
-                result = line.decode('utf-8')
-                theResult = theResult + result.replace("\n","<br/>")
-            print("-- Result:[%s]\n" % theResult)
-            #myProc.stdout.flush()
-            for line in myProc.stderr:
-                #err = str(line.rstrip())
-                error = line.decode('utf-8')
-                theError = theError + error.replace("\n","<br/>")
-            print("-- Error:[%s]\n" % theError)
-            #myProc.stderr.flush()  
-            
-#        result = out.decode(encoding='UTF-8')
-#        theResult = result.replace("\n","<br/>")
-#        error = err.decode(encoding='UTF-8')
-#        theError = error.replace("\n","<br/>")
-
-        print("\nThe output of the compiler-linker-uploader:\n%s\n" % theError)
-        print("\nThe errors :\n%s\n" % theError)
-        print("\nThe Return Code :\n%s\n" % theReturnCode)
-        print(" Done.\n")
-    
-    return render_template('main.html', thePort=myPort, theBoardList=boardList, theBoard=myBoard, theTargetList=targetList, theTarget=myTarget, theOptionList=myOptionList, theOption=myOption, theTempFile=myTempDirectory+separator+myFileName, result=theResult, error=theError)
-
-
     
 # Main page, and process code compile and upload requests
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
     global theResult
     global theError
     global theReturnCode
@@ -343,7 +250,6 @@ def upload():
         
         tmp = request.data
         theCode = tmp.decode(encoding='UTF-8')
-#        theCode = tmp
         theFileName = myTempDirectory + separator + myFileName
         print("The code:\n%s\n" % theCode)
         print("Try to save the code to a local file %s\n" % theFileName)
@@ -352,17 +258,16 @@ def upload():
         try:
             f = open(theFileName, "w")
             print("Trying...\n")
-#        except (OSError, IOError) as err:
-        except:
-            print("Unable to open file for writing: Error: %s\n" % sys.exc_info()[0])
+        except err:
+            print("Unable to open file for writing: IOError: %s\n" % err)
         else:
             try:
                 print("Writing...\n")
                 f.write(theCode)
                 print("Code written to %s\n" % theFileName)
-#            except  (OSError, IOError) as err:
-            except:
-                print("Unable to write in the file %s. Error : %s\n" % {theFileName,  sys.exc_info()[0]})
+#            except IOError, err:
+            except err:
+                print("Unable to write in the file : %s\n" % err)
             finally:
                 f.close()
                 print("File closed.\n")
@@ -370,7 +275,7 @@ def upload():
 
         # arduino --board arduino:avr:nano:cpu=atmega168 --port /dev/ttyACM0 --upload /path/to/sketch/sketch.ino
         compileTime = datetime.now()
-        myCmd = myArduinoExe+" "+myBoardOptions+" "+myBoard+" "+myTargetOption+" "+myTarget+" "+myOption+" "+myCompileAndUploadOption+" "+myTempDirectory+separator+myFileName
+        myCmd = myArduinoExe+" "+myBoardOptions+" "+myBoard+" "+myTargetOption+" "+myTargetRoot+myTarget+" "+myOption+" "+myCompileAndUploadOption+" "+myTempDirectory+separator+myFileName
         print("\nThe shell command:\n%s\n" % myCmd)
 
 #        theResult = os.system(myCmd)
@@ -410,121 +315,12 @@ def upload():
     return render_template('main.html', thePort=myPort, theBoardList=boardList, theBoard=myBoard, theTargetList=targetList, theTarget=myTarget, theOptionList=myOptionList, theOption=myOption, theTempFile=myTempDirectory+separator+myFileName, result=theResult, error=theError)
 
 
-# Main page, and process code compile and upload requests
-@app.route('/', methods=['GET', 'POST'])
-def main_page():
-    global theResult
-    global theError
-    global theReturnCode
-    global myCmd
-    global myFileName
-    global compileTime
-    global myProc
-
-    print(" Call main page.\n")
-    
-    return render_template('main.html', thePort=myPort, theBoardList=boardList, theBoard=myBoard, theTargetList=targetList, theTarget=myTarget, theOptionList=myOptionList, theOption=myOption, theTempFile=myTempDirectory+separator+myFileName, result=theResult, error=theError)
-
-    
-def flaskrun(app, default_host="127.0.0.1", 
-                  default_port=myPort):
-    """
-    Takes a flask.Flask instance and runs it. Parses 
-    command-line flags to configure the app.
-    """
-    global myTarget
-    global targetList
-    global myBoard
-    global boardList
-    global myOption    
-    global myOptionList
-    global myArduinoExe
-    global debugMode
-
-    # Set up the command-line options
-    parser = optparse.OptionParser()
-    parser.add_option("-H", "--host",
-                      help="Hostname of the rDuino_Uploader_Server app " + \
-                           "[default : %s]" % default_host,
-                      default=default_host)
-    parser.add_option("-P", "--port",
-                      help="Ethernet port for the rDuino_Uploader_Server app " + \
-                           "[default : %s]" % default_port,
-                      default=default_port)
-    parser.add_option("-D", "--device",
-                      help="Address of the target to be programmed : " + str(targetList).strip('[]')+ \
-                           "[default : %s]" % myTarget,
-                      default=myTarget)
-    parser.add_option("-B", "--board",
-                      help="The type of board to be programmed : " + str(boardList).strip('[]')+ \
-                           "[default : %s]" % myBoard,
-                      default=myBoard)
-    parser.add_option("-O", "--option",
-                      help="Options to be used for programming and uploading the code : " + str(myOptionList).strip('[]')+ \
-                           "[default : %s]" % myOption,
-                      default=myOption)
-    parser.add_option("-E", "--exec",
-                      help="path and exec to be launched to compile and program the target : " + \
-                           "[default : %s]" % myArduinoExe,
-                      default=myArduinoExe)
-    #parser.add_option("-d", "--debug",
-    #                  help="Debug mode of Falsk. Do not use in production... : " + \
-    #                       "[default : %s]" % debugMode,
-    #                  default=debugMode)
-
-    # Two options useful for debugging purposes, but 
-    # a bit dangerous so not exposed in the help message.
-    parser.add_option("-d", "--debug",
-                      action="store_true", dest="debug",
-                      help=optparse.SUPPRESS_HELP)
-    parser.add_option("-p", "--profile",
-                      action="store_true", dest="profile",
-                      help=optparse.SUPPRESS_HELP)
-
-    options, _ = parser.parse_args()
-    
-    myTarget = options.device
-    myBoard = options.board
-    myOption = options.option
-    myArduinoExe = options.exec
-
-    # If the user selects the profiling option, then we need
-    # to do a little extra setup
-    if options.profile:
-        from werkzeug.contrib.profiler import ProfilerMiddleware
-
-        app.config['PROFILE'] = True
-        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
-                       restrictions=[30])
-        options.debug = True
-
-    print("\nRun the serveur with the following parameters :\n")
-    print("   Host  : %s" % options.host)
-    print("   Port  : %s" % options.port)
-    print("   Device: %s" % myTarget)
-    print("   Board : %s" % myBoard)
-    print("   Option: %s" % myOption)
-    print("   Exec  : %s" % myArduinoExe)
-
-    #app.config['DEBUG'] = True
-    
-    app.run(
-        debug=options.debug,
-        host=options.host,
-        port=int(options.port)
-    )
-    # Start the main server
-    #app.run(port=myPort)    
 
 if __name__ == '__main__':
-    global app
-    
+
     # Create the temp file directory if needed
     if not(os.path.isdir(myTempDirectory)):
         os.system("mkdir "+myTempDirectory)
-    
+        
     # Start the main server
-    flaskrun(app)
-
-
-
+    app.run(port=myPort)
